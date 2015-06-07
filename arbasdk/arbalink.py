bb2c59e0628d0b09c647e5a9d7d7b5e7697801fc
Yoan Mollard <yoan.mollard@inria.fr>
2015-06-07 22:21:05 +0200

    Arbalink: Removed lock used with unthreaded method calls

--- arbasdk/arbalink.py
+++ arbasdk/arbalink.py
@@ -8,7 +8,7 @@
     License: GPL version 3 http://www.gnu.org/licenses/gpl.html
 """
 
-from threading import Thread, Lock
+from threading import Thread
 from serial import Serial
 from sys import stderr
 from time import sleep
@@ -23,7 +23,6 @@ class Arbalink(Thread):
         self.setDaemon(True)
         self.current_device = 0
         self.serial = None
-        self.serial_lock = Lock()
         self.model = None
         self.diminution = diminution
         self.running = True
@@ -41,18 +40,17 @@ class Arbalink(Thread):
     def connect(self):
         success = False
         device = self.config['devices'][self.platform][self.current_device]
-        with self.serial_lock:
-            try:
-                self.serial = Serial(device, self.config['speed'], timeout=0)
-            except Exception, e:
-                print >> stderr, "[Arbalink] Connection to {} at speed {} failed: {}".format(device, self.config['speed'], e.message)
-                self.serial = None
-                self.current_device = (self.current_device+1) % len(self.config['devices'])
-            else:
-                success = True
+        try:
+            self.serial = Serial(device, self.config['speed'], timeout=0)
+        except Exception, e:
+            print >> stderr, "[Arbalink] Connection to {} at speed {} failed: {}".format(device, self.config['speed'], e.message)
+            self.serial = None
+            self.current_device = (self.current_device+1) % len(self.config['devices'])
+        else:
+            success = True
         return success
 
-    def connect_until(self, timeout, num_attempts=20):
+    def connect_forever(self):
         success = False
         while not success:
             success = self.connect()
@@ -66,10 +64,9 @@ class Arbalink(Thread):
 
     def close(self, reason='unknown'):
         self.running = False
-        with self.serial_lock:
-            if self.serial:
-                self.serial.close()
-                self.serial = None
+        if self.serial:
+            self.serial.close()
+            self.serial = None
 
     def run(self):
         def __limit(v):
@@ -77,33 +74,32 @@ class Arbalink(Thread):
 
         while(self.running):
             reconnect = True
-            with self.serial_lock:
-                if self.serial and self.serial.isOpen():
-                    if self.model:
-                        array = bytearray(' '*(self.model.get_height()*self.model.get_width()*3))
-                        self.model.lock()
-                        for h in range(self.model.get_height()):
-                            for w in range(self.model.get_width()):
-                                try:
-                                    idx = self.config['mapping'][h][w]*3 # = mapping shift by 3 colors
-                                except IndexError, e:
-                                    raise Exception('Incorrect mapping, please check your configuration file, arbalink exiting...')
-                                    self.close('config error')
-                                else:
-                                    pixel = self.model.get_pixel(h, w)
-                                    array[idx] = __limit(pixel.r*self.diminution)
-                                    array[idx+1] = __limit(pixel.g*self.diminution)
-                                    array[idx+2] = __limit(pixel.b*self.diminution)
-                        try:
-                            self.serial.write(array) # Write the whole rgb-matrix
-                            #self.serial.readline() # Wait Arduino's feedback
-                        except:
-                            pass
-                        else:
-                            reconnect = False
-                        # serial I/Os while a mutex is locked, oh oh
-                        self.model.unlock()
+            if self.serial and self.serial.isOpen():
+                if self.model:
+                    array = bytearray(' '*(self.model.get_height()*self.model.get_width()*3))
+                    self.model.lock()
+                    for h in range(self.model.get_height()):
+                        for w in range(self.model.get_width()):
+                            try:
+                                idx = self.config['mapping'][h][w]*3 # = mapping shift by 3 colors
+                            except IndexError, e:
+                                raise Exception('Incorrect mapping, please check your configuration file, arbalink exiting...')
+                                self.close('config error')
+                            else:
+                                pixel = self.model.get_pixel(h, w)
+                                array[idx] = __limit(pixel.r*self.diminution)
+                                array[idx+1] = __limit(pixel.g*self.diminution)
+                                array[idx+2] = __limit(pixel.b*self.diminution)
+                    try:
+                        self.serial.write(array) # Write the whole rgb-matrix
+                        #self.serial.readline() # Wait Arduino's feedback
+                    except:
+                        pass
+                    else:
+                        reconnect = False
+                    # serial I/Os while a mutex is locked, oh oh
+                    self.model.unlock()
             if reconnect:
-                self.connect_until(60)
+                self.connect_forever()
             else:
                 self.rate.sleep()
