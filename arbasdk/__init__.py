6041a656193804b1914d24f5895e81534e66f6ae
Yoan Mollard <yoan.mollard@inria.fr>
2015-07-20 12:46:13 +0200

    Arbatext into the module

--- arbasdk/__init__.py
+++ arbasdk/__init__.py
@@ -3,5 +3,6 @@ from arbapixel import Arbapixel, hsv
 from arbalink import Arbalink
 from arbalet import Arbalet
 from arbasim import Arbasim
+from arbatext import Arbatext
 from arbapp import Arbapp
 from rate import Rate
