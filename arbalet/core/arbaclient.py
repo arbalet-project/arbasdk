98661efb2905186a8d6cba68c5c0164d4587e669
Yoan Mollard <yoan.mollard@inria.fr>
2017-01-22 15:21:00 +0100

    [UNSTABLE] Quick-and-dirty patch to start the data bus

--- arbalet/core/arbaclient.py
+++ arbalet/core/arbaclient.py
@@ -11,6 +11,7 @@
 import zmq
 from threading import Thread
 from . rate import Rate
+from json import loads
 
 __all__ = ['Arbaclient']
 
@@ -36,14 +37,28 @@ class Arbaclient(Thread):
             self.sender = self.context.socket(zmq.PAIR)
             self.sender.connect("tcp://{}:{}".format(self.server, self.port))
 
+        self.bus = self.context.socket(zmq.SUB)
+        self.bus.setsockopt(zmq.SUBSCRIBE, '')
+        self.bus.connect("tcp://{}:{}".format(self.server, 33105))
+
     def send_model(self):
         self.sender.send_json(self.arbalet.end_model.to_json())
 
     def receive_touch(self):
-        touch_frame = self.sender.recv_json()
-        touch_int = touch_frame[0]
-        touch_booleans = touch_frame[1]
-        self.arbalet.touch.create_event(touch_int, touch_booleans)
+        try:
+            joystick = loads(self.bus.recv_json(flags=zmq.NOBLOCK))
+        except zmq.error.ZMQError as e:
+            pass
+        else:
+            print("Received" + str(joystick))
+            mapper = {'left':4, 'down':2, 'right':0, 'up':5}
+            joystick['id'] = mapper[joystick['id']]
+            print "added", joystick
+            self.arbalet.touch._touch_events.append(joystick)
+            #touch_frame = self.sender.recv_json()
+            #touch_int = touch_frame[0]
+            #touch_booleans = touch_frame[1]
+            #self.arbalet.touch.create_event(touch_int, touch_booleans)
 
     def close(self, reason='unknown'):
         self.running = False
