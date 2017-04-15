228eae045a618eb051eceedd4706c8685f427c80
Yoan Mollard <yoan.mollard@inria.fr>
2017-04-15 20:03:20 +0200

    Bugfix get_hw_link

--- arbalet/display/hardware/__init__.py
+++ arbalet/display/hardware/__init__.py
@@ -1,15 +1,15 @@
 from os.path import realpath as _realpath
 from .simulator import Simulator
 
-def get_hardware_link(host, hardware_config, stop_event=None):
+def get_hardware_link(host, hardware_config):
     if hardware_config["controller"] in ["arduino"]:
         from .arduino import ArduinoDisplayServer as _ArduinoLink
-        return _ArduinoLink(host, hardware_config, stop_event=stop_event)
+        return _ArduinoLink(host, hardware_config)
     if hardware_config["controller"] in ["rpi_spi", "raspberrypi", "pi", 'spi"']:
         from .raspberrypi import RPiSPIDisplayServer as _RPiLinkSPI
-        return _RPiLinkSPI(host, hardware_config, stop_event=stop_event)
+        return _RPiLinkSPI(host, hardware_config)
     if hardware_config["controller"] in ["rpi_pwm", "pwm"]:
         from .raspberrypi import RPiPWMDisplayServer as _RPiLinkPWM
-        return _RPiLinkPWM(host, hardware_config, stop_event=stop_event)
+        return _RPiLinkPWM(host, hardware_config)
     raise NotImplementedError("{} knows no implementation of link type \"{}\" specified in config file".format(_realpath(__file__), hardware_config["controller"]))
 
