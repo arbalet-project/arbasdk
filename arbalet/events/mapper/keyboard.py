06856611a84b2d92931b2e4817356c519ffaadcc
Yoan Mollard <yoan.mollard@inria.fr>
2017-04-16 02:01:34 +0200

    Publish F1-F10 as events

--- arbalet/events/mapper/keyboard.py
+++ arbalet/events/mapper/keyboard.py
@@ -1,6 +1,6 @@
 from ...config import ConfigReader
 from .abstract import AbstractMapper
-from pygame import K_UP, K_DOWN, K_LEFT, K_RIGHT
+from pygame import K_UP, K_DOWN, K_LEFT, K_RIGHT, K_F1, K_F2, K_F3, K_F4, K_F5, K_F6, K_F7, K_F8, K_F9, K_F10, K_SPACE, K_RETURN
 from time import time
 
 
@@ -9,7 +9,9 @@ class KeyboardMapper(AbstractMapper):
         super(KeyboardMapper, self).__init__()
         config_reader = ConfigReader()
         self.config = config_reader.hardware['touch']
-        self.mapping = {K_UP: 'up', K_DOWN:'down', K_RIGHT:'right', K_LEFT:'left'}
+        self.mapping = {K_UP: 'up', K_DOWN:'down', K_RIGHT:'right', K_LEFT:'left', K_F1: 'F1', K_F2: 'F2', K_F3: 'F3',
+                        K_F4: 'F4', K_F5: 'F5', K_F6: 'F6', K_F7: 'F7', K_F8: 'F8', K_F9: 'F9', K_F10: 'F10',
+                        K_RETURN: 'action', K_SPACE: 'action'}
 
     def map(self, raw_event):
         if raw_event['key'] in self.mapping:
