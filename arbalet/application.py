a3e09a4649bc92530ec4ac5c85eed92aa429946c
Yoan Mollard <yoan.mollard@inria.fr>
2017-04-17 20:25:41 +0200

    SDK 4: stop passing the parser in parameter

--- arbalet/application.py
+++ arbalet/application.py
@@ -17,86 +17,63 @@ from .core.servers import Servers
 from argparse import ArgumentParser
 
 
-__all__ = ['Application']
+__all__ = ['Application', 'get_application_parser']
+
+def get_application_parser(parser=None):
+    if parser is None:
+        parser = ArgumentParser(description='This script runs on Arbalet and allows the following arguments:')
+
+    parser.add_argument('-s', '--server',
+                        type=str,
+                        nargs='?',
+                        const=True,
+                        default='localhost',
+                        help='IP or hostname of the D-Bus, Display and Event servers (ex: myserver.local, 192.168.0.15, ...)'
+                             'Do not provide port, it is specified in the D-Bus configuration file')
+
+    parser.add_argument('-b', '--brightness',
+                        type=float,
+                        default=1,
+                        help='Brightness, intensity of hardware LEDs between 0.0 (all LEDs off) and 1.0 (all LEDs at full brightness)')
+
+    parser.add_argument('-nt', '--no-touch',
+                        action='store_const',
+                        const=True,
+                        default=False,
+                        help='Disable the touch feature. This option has no influence on apps that are not touch-compatible')
+
+    parser.add_argument('-o', '--standalone',
+                        action='store_const',
+                        const=True,
+                        default=False,
+                        help='Run this app in standalone mode by also starting all background servers')
+
+    parser = get_config_parser(parser)
+    parser = get_display_parser(parser)
+    return parser
+
 
 class Application(object):
-    app_declared = False  # True when an Application has been instanciated
-
-    def __init__(self, argparser=None, touch_mode='off'):
-        if Application.app_declared:
-            raise RuntimeError('Application can be instanciated only once')
-
-        Application.app_declared = True
-        self.read_args(argparser)
-        self.events = EventClient(host=self.args.server)
-        self._servers = Servers(self.args.hardware, not self.args.hardware)
+    def __init__(self, host='127.0.0.1', hardware=False, simulation=True, standalone=False, touch_mode='off', **kwargs):
+        self.events = EventClient(host=host)
+        self._servers = Servers(hardware, simulation)
         self._config = ConfigReader()
+        self._standalone = standalone
         self.width = self._config.hardware['width']
         self.height = self._config.hardware['height']
         self.model = Model(self.height, self.width)
-        self._client = DisplayClient(self.model, self.args.server)
-
-    def is_interactive(self):
-        """
-        :return: True if the code is running interactively with IPYTHON, False otherwise
-        """
-        try:
-            __IPYTHON__
-        except NameError:
-            return False
-        else:
-            return True
-
-    def read_args(self, argparser):
-
-        if argparser:
-            parser = argparser
-        else:
-            parser = ArgumentParser(description='This script runs on Arbalet and allows the following arguments:')
-
-        parser.add_argument('-s', '--server',
-                            type=str,
-                            nargs='?',
-                            const=True,
-                            default='localhost',
-                            help='IP or hostname of the D-Bus, Display and Event servers (ex: myserver.local, 192.168.0.15, ...)'
-                                 'Do not provide port, it is specified in the D-Bus configuration file')
-        parser.add_argument('-b', '--brightness',
-                            type=float,
-                            default=1,
-                            help='Brightness, intensity of hardware LEDs between 0.0 (all LEDs off) and 1.0 (all LEDs at full brightness)')
-        parser.add_argument('-f', '--factor_sim',
-                            type=int,
-                            default=40,
-                            help='Size of the simulated pixels')
-        parser.add_argument('-nt', '--no-touch',
-                            action='store_const',
-                            const=True,
-                            default=False,
-                            help='Disable the touch feature. This option has no influence on apps that are not touch-compatible')
-        parser.add_argument('-o', '--standalone',
-                            action='store_const',
-                            const=True,
-                            default=False,
-                            help='Run this app in standalone mode by also starting all background servers')
-
-        parser = get_config_parser(parser)
-        parser = get_display_parser(parser)
-
-        # We parse args normally if running in non-interactive mode, otherwise we ignore args to avoid conflicts with ipython
-        self.args = parser.parse_args([] if self.is_interactive() else None)
-
+        self._client = DisplayClient(self.model, host)
 
     def run(self):
         raise NotImplementedError("Application.run() must be overidden")
 
     def start(self):
         try:
-            if self.args.standalone:
+            if self._standalone:
                 self._servers.start()
             self.run()
         except KeyboardInterrupt:
-            if self.args.standalone:
+            if self._standalone:
                 self._servers.stop(is_keyboard_interrupt=True)
         else:
             self._servers.stop(is_keyboard_interrupt=False)
